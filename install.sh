4142d5801947aa59af1c8ca2495c133c2d779868
thepante <panterua@hotmail.com>
2019-02-02 03:48:04 -0300

    update

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-sudo cp ./extractor/vera.py "~/.local/lib/python2.7/dist-packages/youtube_dl/extractor/vera.py" && 
+sudo cp ./extractor/vera.py "~/.local/lib/python2.7/site-packages/youtube_dl/extractor/vera.py" && 
 echo "[1/5] Copiado a Python2.7..." &&
 cp ./extractor/vera.py "/usr/lib/python3/dist-packages/youtube_dl/extractor/vera.py" && 
 echo "[2/5] Copiado a Python3..." &&
-echo 'from .vera import VeraIE' >> ~/.local/lib/python2.7/dist-packages/youtube_dl/extractor/extractors.py &&
+echo 'from .vera import VeraIE' >> ~/.local/lib/python2.7/site-packages/youtube_dl/extractor/extractors.py &&
 echo "[3/5] Incluido en youtube_dl Python2.7 ..." &&
 echo 'from .vera import VeraIE' >> /usr/lib/python3/dist-packages/youtube_dl/extractor/extractors.py &&
 echo "[4/5] Incluido en youtube_dl Python3 ..." &&
