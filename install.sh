5b7386ff862f2a321ec55c3b162f37d7cf0f1c9c
thepante <panterua@hotmail.com>
2019-02-02 14:57:58 -0300

    update

--- install.sh
+++ install.sh
@@ -24,6 +24,6 @@ then
   	echo "Integrado a YT-DL Python3 ..."
 fi
 
-cd ${HOME} && rm -rf ~/.temp/ytdl-vera &&
+cd .. && rm -rf ytdl-vera &&
 echo "Autodestrucción de este repo temporal..." &&
 echo "[ ✓ ] Listo, probá youtube-dl."
